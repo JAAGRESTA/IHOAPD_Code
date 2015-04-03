957fcbc1eb4c664103f23b7fcd375c9f7b008176
JAAGRESTA <jagresta2@gmail.com>
2015-04-03 09:38:01 -0400

    Initial Code

---
+++ IHOAPD_Code.ino
@@ -0,0 +1,18 @@
+/* IHOAP - Infrared Head-Operated Assistive Pointing Device Arduino Code
+*  ECE 2799 Team 8: Joe Agresta, Giselle Verbera, and Sean Watson
+* 
+*/
+
+
+
+//one-time code
+void setup()
+{
+  
+}
+
+//main code
+void loop()
+{
+  
+}
