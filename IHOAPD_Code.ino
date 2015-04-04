1f023befb93aa2fbe8a08047395c0e8052710731
JAAGRESTA <jagresta2@gmail.com>
2015-04-04 10:48:23 -0400

    Test Code Added.
    Precursor to etch-a-sketch proof of concept for testing.

--- IHOAPD_Code.ino
+++ IHOAPD_Code.ino
@@ -3,16 +3,36 @@
 * 
 */
 
+#define leftControlPin A0
+#define rightControlPin A1
+int xPos = 0, yPos = 0;
 
 
 //one-time code
 void setup()
 {
-  
+  serial.begin(9600);
+  pinMode(leftControlPin, INPUT);
+  pinMode(rightControlPin, INPUT);
+
 }
 
 //main code
 void loop()
 {
-  
+  xposPos = getLeftVal();
+  y = getRightVal();
+  serial.println("("+ x + "," + y + ")")
+}
+
+//reads left potentiometer value for testing
+void getLeftVal()
+{
+	analogRead(leftControlPin);
+}
+
+//reads right potentiometer value for testing
+void getRightVal()
+{
+	analogRead(rightControlPin);
 }
