43db7a11ec477bef9ec32db97db1fac3a5e14c22
JAAGRESTA <jagresta2@gmail.com>
2015-04-04 12:41:22 -0400

    Reads pots values as X and Y.

--- IHOAPD_Code.ino
+++ IHOAPD_Code.ino
@@ -2,7 +2,7 @@
 *  ECE 2799 Team 8: Joe Agresta, Giselle Verbera, and Sean Watson
 * 
 */
-
+#include Mouse
 #define leftControlPin A0
 #define rightControlPin A1
 int xPos = 0, yPos = 0;
@@ -11,7 +11,7 @@ int xPos = 0, yPos = 0;
 //one-time code
 void setup()
 {
-  serial.begin(9600);
+  Serial.begin(9600);
   pinMode(leftControlPin, INPUT);
   pinMode(rightControlPin, INPUT);
 
@@ -20,19 +20,23 @@ void setup()
 //main code
 void loop()
 {
-  xposPos = getLeftVal();
-  y = getRightVal();
-  serial.println("("+ x + "," + y + ")")
+ 	  getLeftVal();
+ 	  getRightVal();
+ 	  Serial.print("(");
+ 	  Serial.print(xPos);
+ 	  Serial.print(",");
+  	  Serial.print(yPos);
+  	  Serial.println(")");
 }
 
 //reads left potentiometer value for testing
 void getLeftVal()
 {
-	analogRead(leftControlPin);
+	xPos = analogRead(leftControlPin);
 }
 
 //reads right potentiometer value for testing
 void getRightVal()
 {
-	analogRead(rightControlPin);
+	yPos = analogRead(rightControlPin);
 }
